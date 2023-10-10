019eb944cdb4776156bdc8a68a5a2073c7ac429e
Chesed Kim <starmireu@gmail.com>
2023-10-10 15:09:09 +0900

    swagger url 수정

--- core/urls.py
+++ core/urls.py
@@ -27,7 +27,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     # path('', home, name='home'),
-    # 초기화면 swagger 로 지정.
+    # swagger 경로.
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     
